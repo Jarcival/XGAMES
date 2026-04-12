af81307a656d267cbc2c667110b57ebaf566e10e
Jean Puentes <jeandragneel15@gmail.com>
2026-04-12 16:07:13 -0600

    Componentes y servicios(API)

---
+++ frontend/src/app/components/alta-prod/alta-prod.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alta-prod',
+  imports: [],
+  templateUrl: './alta-prod.html',
+  styleUrl: './alta-prod.css',
+})
+export class AltaProd {
+
+}
