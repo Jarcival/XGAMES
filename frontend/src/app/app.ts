d58129cd93832296a73fa30a0d7c3b0387a51bc8
Juan Daniel <arriagaverajuandaniel@gmail.com>
2026-04-13 09:52:09 -0600

    Construir vistas: inicio y catalogo

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('frontend');
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
